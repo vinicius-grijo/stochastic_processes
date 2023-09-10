ce50a3786bc8a980c94d16fd7dd522ffc857a50d
vinicius-grijo <vrl.grijo@gmail.com>
2023-09-10 18:47:40 -0300

    random_walk_around_the_clock atualizado

--- random_walk_around_the_clock.py
+++ random_walk_around_the_clock.py
@@ -31,42 +31,50 @@ def random_walk_around_the_clock( alpha_1=1, alpha_2=1, sigma_1=0.1, sigma_2=0.1
 if __name__ == '__main__':
     alpha_1 = 1
     alpha_2 = 1
-    sigma_1 = 0.1
-    sigma_2 = 0.1
+    sigma_1 = 0.5
+    sigma_2 = 0.5
 
     # time interval
     T = int( np.ceil(2*np.pi) )
     # number of times [0,1] is divided
-    N = 50
+    N = 5000
 
-    process_1 = random_walk_around_the_clock(N=N, T=T)
-    process_2 = random_walk_around_the_clock(N=N, T=T)
-    process_3 = random_walk_around_the_clock(N=N, T=T)
-    process_4 = random_walk_around_the_clock(N=N, T=T)
-    process_5 = random_walk_around_the_clock(N=N, T=T)
+    path_1 = random_walk_around_the_clock(alpha_1=alpha_1, alpha_2=alpha_2, sigma_1=sigma_1, sigma_2=sigma_2, T=T, N=N)
+    path_2 = random_walk_around_the_clock(alpha_1=alpha_1, alpha_2=alpha_2, sigma_1=sigma_1, sigma_2=sigma_2, T=T, N=N)
+    path_3 = random_walk_around_the_clock(alpha_1=alpha_1, alpha_2=alpha_2, sigma_1=sigma_1, sigma_2=sigma_2, T=T, N=N)
+    path_4 = random_walk_around_the_clock(alpha_1=alpha_1, alpha_2=alpha_2, sigma_1=sigma_1, sigma_2=sigma_2, T=T, N=N)
+    path_5 = random_walk_around_the_clock(alpha_1=alpha_1, alpha_2=alpha_2, sigma_1=sigma_1, sigma_2=sigma_2, T=T, N=N)
 
-    fig=plt.figure()
+    # plt.xlim( -2, 2 )
+    # plt.ylim( -2, 2 )
     plt.title('Random Walk Around the Clock')
-    l_1, = plt.plot( [], [], label='process_1' )
-    l_2, = plt.plot( [], [], label='process_2' )
-    l_3, = plt.plot( [], [], label='process_3' )
-    l_4, = plt.plot( [], [], label='process_4' )
-    l_5, = plt.plot( [], [], label='process_5' )
-    plt.xlim( -2, 2 )
-    plt.ylim( -2, 2 )
-    plt.legend()
-    metadata = dict( title='random_walk_around_the_clock', artist='me' )
-    writer = PillowWriter(fps=(N*T+1), metadata=metadata)
+    plt.plot(path_1[:,0], path_1[:,1], linewidth=1, label='path_1')
+    plt.plot(path_2[:,0], path_2[:,1], linewidth=1, label='path_2')
+    plt.plot(path_3[:,0], path_3[:,1], linewidth=1, label='path_3')
+    plt.plot(path_4[:,0], path_4[:,1], linewidth=1, label='path_4')
+    plt.plot(path_5[:,0], path_5[:,1], linewidth=1, label='path_5')
+#     plt.legend()
+    plt.savefig('random_walk_around_the_clock.png')
+    plt.show()
 
-    with writer.saving(fig, 'random_walk_around_the_clock.gif', 100):
-        for i in range(N*T+1):
-            l_1.set_data( process_1[0:i, 0], process_1[0:i, 1] )
-            l_2.set_data( process_2[0:i, 0], process_2[0:i, 1] )
-            l_3.set_data( process_3[0:i, 0], process_3[0:i, 1] )
-            l_4.set_data( process_4[0:i, 0], process_4[0:i, 1] )
-            l_5.set_data( process_5[0:i, 0], process_5[0:i, 1] )
-            writer.grab_frame()
+    # fig=plt.figure()
+    # plt.title('Random Walk Around the Clock')
+    # l_1, = plt.plot( [], [], label='process_1' )
+    # l_2, = plt.plot( [], [], label='process_2' )
+    # l_3, = plt.plot( [], [], label='process_3' )
+    # l_4, = plt.plot( [], [], label='process_4' )
+    # l_5, = plt.plot( [], [], label='process_5' )
+    # plt.xlim( -2, 2 )
+    # plt.ylim( -2, 2 )
+    # plt.legend()
+    # metadata = dict( title='random_walk_around_the_clock', artist='me' )
+    # writer = PillowWriter(fps=(N*T+1), metadata=metadata)
 
-
-    # plt.plot( X[:,0], X[:,1] )
-    # plt.show()
+    # with writer.saving(fig, 'random_walk_around_the_clock.gif', 100):
+    #     for i in range(N*T+1):
+    #         l_1.set_data( path_1[0:i, 0], path_1[0:i, 1] )
+    #         l_2.set_data( path_2[0:i, 0], path_2[0:i, 1] )
+    #         l_3.set_data( path_3[0:i, 0], path_3[0:i, 1] )
+    #         l_4.set_data( path_4[0:i, 0], path_4[0:i, 1] )
+    #         l_5.set_data( path_5[0:i, 0], path_5[0:i, 1] )
+    #         writer.grab_frame()
